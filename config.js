65d19bc8bc191b3790e8ddb6b7b95a2123e18950
nimetbuse07 <101000938+nimetbuse07@users.noreply.github.com>
2025-08-21 10:48:05 +0300

    İlk yükleme: beyazEsyaWebSitesi

---
+++ config.js
@@ -0,0 +1,7 @@
+const AppConfig = {
+    companyName: "Teknik Servis",
+    phoneNumber: "+900000000000",
+    email: "info@teknikservis.com",
+    addressLocality: "Şehir", 
+    addressCountry: "TR",   
+};
